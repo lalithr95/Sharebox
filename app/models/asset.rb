4aff1bc2ba4197714d1d9252a498c8648db22a26
Lalith <lalithr95@gmail.com>
2015-12-03 19:30:15 +0530

    Download routes and ignores

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,7 +1,12 @@
 class Asset < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :uploaded_file, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :uploaded_file, url: 'download/assets/:id', path: 'assets/:id/:basename.:extension', styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :uploaded_file, content_type: /\Aimage\/.*\Z/
   validates_attachment_presence :uploaded_file
   #TODO validate attachment size for uploaded_file
+
+  def file_name
+  	# TO use directly asset.file_name instead of asset.uploaded_file_file_name
+  	uploaded_file_file_name
+  end
 end
