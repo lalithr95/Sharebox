a4e2576813bd375fae1cc6dd40fa9cc2fa5a2818
Lalith <lalithr95@gmail.com>
2015-12-06 19:57:47 +0530

    list folders in index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
   	if user_signed_in?
+  		@folders = current_user.folders.order('name DESC')
   		@assets = current_user.assets.order('uploaded_file_file_name DESC')
   	end
   end
