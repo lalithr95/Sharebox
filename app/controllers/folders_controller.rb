cc36c125a665e6233cf023ef0b7e04e7fbe4a78b
Lalith <lalithr95@gmail.com>
2015-12-07 21:00:42 +0530

    Remove user id and parent id

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -70,6 +70,6 @@ class FoldersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def folder_params
-      params.require(:folder).permit(:name, :parent_id, :user_id)
+      params.require(:folder).permit(:name)
     end
 end
