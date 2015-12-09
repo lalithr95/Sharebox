f79759cca54282896fbef92e71db677f77b4e5fc
Lalith <lalithr95@gmail.com>
2015-12-09 20:46:27 +0530

    Routes for folder browse

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # download route
   match 'master/download/assets/:id' => 'assets#download', as: 'download', via: :get
   devise_for :users
+  match 'browse/:folder_id' => 'home#browse', as: 'browse', via: :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
